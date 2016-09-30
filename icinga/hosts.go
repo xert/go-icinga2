8ca5d714ed305239cf502d4749d7bf5551fbd54c
Robert Kánia <rk@redb.cz>
2016-09-30 21:42:05 +0200

    Simple test

--- icinga/hosts.go
+++ icinga/hosts.go
@@ -17,98 +17,98 @@ type hostResults struct {
 }
 
 type Host struct {
-	Name                  string      `json:"__name"`
-	Acknowledgement       json.Number `json:"acknowledgement"`
-	AcknowledgementExpiry json.Number `json:"acknowledgement_expiry"`
-	ActionURL             string      `json:"action_url"`
-	Active                bool        `json:"active"`
-	Address               string      `json:"address"`
-	Address6              string      `json:"address6"`
-	CheckAttempt          json.Number `json:"check_attempt"`
-	CheckCommand          string      `json:"check_command"`
-	CheckInterval         json.Number `json:"check_interval"`
-	CheckPeriod           string      `json:"check_period"`
-	CheckTimeout          interface{} `json:"check_timeout"`
-	CommandEndpoint       string      `json:"command_endpoint"`
-	DisplayName           string      `json:"display_name"`
-	DowntimeDepth         json.Number `json:"downtime_depth"`
-	EnableActiveChecks    bool        `json:"enable_active_checks"`
-	EnableEventHandler    bool        `json:"enable_event_handler"`
-	EnableFlapping        bool        `json:"enable_flapping"`
-	EnableNotifications   bool        `json:"enable_notifications"`
-	EnablePassiveChecks   bool        `json:"enable_passive_checks"`
-	EnablePerfdata        bool        `json:"enable_perfdata"`
-	EventCommand          string      `json:"event_command"`
-	Flapping              bool        `json:"flapping"`
-	FlappingLastChange    float64     `json:"flapping_last_change"`
-	FlappingNegative      json.Number `json:"flapping_negative"`
-	FlappingPositive      json.Number `json:"flapping_positive"`
-	FlappingThreshold     json.Number `json:"flapping_threshold"`
-	ForceNextCheck        bool        `json:"force_next_check"`
-	ForceNextNotification bool        `json:"force_next_notification"`
-	Groups                []string    `json:"groups"`
-	HaMode                json.Number `json:"ha_mode"`
-	IconImage             string      `json:"icon_image"`
-	IconImageAlt          string      `json:"icon_image_alt"`
-	LastCheck             float64     `json:"last_check"`
-	LastCheckResult       struct {
-		Active          bool        `json:"active"`
-		CheckSource     string      `json:"check_source"`
-		Command         []string    `json:"command"`
-		ExecutionEnd    float64     `json:"execution_end"`
-		ExecutionStart  float64     `json:"execution_start"`
-		ExitStatus      json.Number `json:"exit_status"`
-		Output          string      `json:"output"`
-		PerformanceData []string    `json:"performance_data"`
-		ScheduleEnd     float64     `json:"schedule_end"`
-		ScheduleStart   float64     `json:"schedule_start"`
-		State           json.Number `json:"state"`
-		Type            string      `json:"type"`
-		VarsAfter       struct {
-			Attempt   json.Number `json:"attempt"`
-			Reachable bool        `json:"reachable"`
-			State     json.Number `json:"state"`
-			StateType json.Number `json:"state_type"`
-		} `json:"vars_after"`
-		VarsBefore struct {
-			Attempt   json.Number `json:"attempt"`
-			Reachable bool        `json:"reachable"`
-			State     json.Number `json:"state"`
-			StateType json.Number `json:"state_type"`
-		} `json:"vars_before"`
-	} `json:"last_check_result"`
-	LastHardState        json.Number `json:"last_hard_state"`
-	LastHardStateChange  float64     `json:"last_hard_state_change"`
-	LastReachable        bool        `json:"last_reachable"`
-	LastState            json.Number `json:"last_state"`
-	LastStateChange      float64     `json:"last_state_change"`
-	LastStateDown        float64     `json:"last_state_down"`
-	LastStateType        json.Number `json:"last_state_type"`
-	LastStateUnreachable json.Number `json:"last_state_unreachable"`
-	LastStateUp          float64     `json:"last_state_up"`
-	MaxCheckAttempts     json.Number `json:"max_check_attempts"`
-	NextCheck            float64     `json:"next_check"`
-	Notes                string      `json:"notes"`
-	NotesURL             string      `json:"notes_url"`
-	OriginalAttributes   interface{} `json:"original_attributes"`
-	Package              string      `json:"package"`
-	Paused               bool        `json:"paused"`
-	RetryInterval        json.Number `json:"retry_interval"`
-	State                json.Number `json:"state"`
-	StateType            json.Number `json:"state_type"`
-	Templates            []string    `json:"templates"`
-	Type                 string      `json:"type"`
-	Vars                 struct {
+	Name                  string      `json:"__name,omitempty"`
+	Acknowledgement       json.Number `json:"acknowledgement,omitempty"`
+	AcknowledgementExpiry json.Number `json:"acknowledgement_expiry,omitempty"`
+	ActionURL             string      `json:"action_url,omitempty"`
+	Active                bool        `json:"active,omitempty"`
+	Address               string      `json:"address,omitempty"`
+	Address6              string      `json:"address6,omitempty"`
+	CheckAttempt          json.Number `json:"check_attempt,omitempty"`
+	CheckCommand          string      `json:"check_command,omitempty"`
+	CheckInterval         json.Number `json:"check_interval,omitempty"`
+	CheckPeriod           string      `json:"check_period,omitempty"`
+	CheckTimeout          interface{} `json:"check_timeout,omitempty"`
+	CommandEndpoint       string      `json:"command_endpoint,omitempty"`
+	DisplayName           string      `json:"display_name,omitempty"`
+	DowntimeDepth         json.Number `json:"downtime_depth,omitempty"`
+	EnableActiveChecks    bool        `json:"enable_active_checks,omitempty"`
+	EnableEventHandler    bool        `json:"enable_event_handler,omitempty"`
+	EnableFlapping        bool        `json:"enable_flapping,omitempty"`
+	EnableNotifications   bool        `json:"enable_notifications,omitempty"`
+	EnablePassiveChecks   bool        `json:"enable_passive_checks,omitempty"`
+	EnablePerfdata        bool        `json:"enable_perfdata,omitempty"`
+	EventCommand          string      `json:"event_command,omitempty"`
+	Flapping              bool        `json:"flapping,omitempty"`
+	FlappingLastChange    float64     `json:"flapping_last_change,omitempty"`
+	FlappingNegative      json.Number `json:"flapping_negative,omitempty"`
+	FlappingPositive      json.Number `json:"flapping_positive,omitempty"`
+	FlappingThreshold     json.Number `json:"flapping_threshold,omitempty"`
+	ForceNextCheck        bool        `json:"force_next_check,omitempty"`
+	ForceNextNotification bool        `json:"force_next_notification,omitempty"`
+	Groups                []string    `json:"groups,omitempty"`
+	HaMode                json.Number `json:"ha_mode,omitempty"`
+	IconImage             string      `json:"icon_image,omitempty"`
+	IconImageAlt          string      `json:"icon_image_alt,omitempty"`
+	LastCheck             float64     `json:"last_check,omitempty"`
+	LastCheckResult       *struct {
+		Active          bool        `json:"active,omitempty"`
+		CheckSource     string      `json:"check_source,omitempty"`
+		Command         []string    `json:"command,omitempty"`
+		ExecutionEnd    float64     `json:"execution_end,omitempty"`
+		ExecutionStart  float64     `json:"execution_start,omitempty"`
+		ExitStatus      json.Number `json:"exit_status,omitempty"`
+		Output          string      `json:"output,omitempty"`
+		PerformanceData []string    `json:"performance_data,omitempty"`
+		ScheduleEnd     float64     `json:"schedule_end,omitempty"`
+		ScheduleStart   float64     `json:"schedule_start,omitempty"`
+		State           json.Number `json:"state,omitempty"`
+		Type            string      `json:"type,omitempty"`
+		VarsAfter       *struct {
+			Attempt   json.Number `json:"attempt,omitempty"`
+			Reachable bool        `json:"reachable,omitempty"`
+			State     json.Number `json:"state,omitempty"`
+			StateType json.Number `json:"state_type,omitempty"`
+		} `json:"vars_after,omitempty"`
+		VarsBefore *struct {
+			Attempt   json.Number `json:"attempt,omitempty"`
+			Reachable bool        `json:"reachable,omitempty"`
+			State     json.Number `json:"state,omitempty"`
+			StateType json.Number `json:"state_type,omitempty"`
+		} `json:"vars_before,omitempty"`
+	} `json:"last_check_result,omitempty"`
+	LastHardState        json.Number `json:"last_hard_state,omitempty"`
+	LastHardStateChange  float64     `json:"last_hard_state_change,omitempty"`
+	LastReachable        bool        `json:"last_reachable,omitempty"`
+	LastState            json.Number `json:"last_state,omitempty"`
+	LastStateChange      float64     `json:"last_state_change,omitempty"`
+	LastStateDown        float64     `json:"last_state_down,omitempty"`
+	LastStateType        json.Number `json:"last_state_type,omitempty"`
+	LastStateUnreachable json.Number `json:"last_state_unreachable,omitempty"`
+	LastStateUp          float64     `json:"last_state_up,omitempty"`
+	MaxCheckAttempts     json.Number `json:"max_check_attempts,omitempty"`
+	NextCheck            float64     `json:"next_check,omitempty"`
+	Notes                string      `json:"notes,omitempty"`
+	NotesURL             string      `json:"notes_url,omitempty"`
+	OriginalAttributes   interface{} `json:"original_attributes,omitempty"`
+	Package              string      `json:"package,omitempty"`
+	Paused               bool        `json:"paused,omitempty"`
+	RetryInterval        json.Number `json:"retry_interval,omitempty"`
+	State                json.Number `json:"state,omitempty"`
+	StateType            json.Number `json:"state_type,omitempty"`
+	Templates            []string    `json:"templates,omitempty"`
+	Type                 string      `json:"type,omitempty"`
+	Vars                 *struct {
 		Notification struct {
 			Mail struct {
-				Groups []string `json:"groups"`
-			} `json:"mail"`
-		} `json:"notification"`
-		Os string `json:"os"`
-	} `json:"vars"`
-	Version  float64 `json:"version"`
-	Volatile bool    `json:"volatile"`
-	Zone     string  `json:"zone"`
+				Groups []string `json:"groups,omitempty"`
+			} `json:"mail,omitempty"`
+		} `json:"notification,omitempty"`
+		Os string `json:"os,omitempty"`
+	} `json:"vars,omitempty"`
+	Version  float64 `json:"version,omitempty"`
+	Volatile bool    `json:"volatile,omitempty"`
+	Zone     string  `json:"zone,omitempty"`
 }
 
 // Get a single Host.
--- icinga/icinga_test.go
+++ icinga/icinga_test.go
@@ -1,9 +1,15 @@
 package icinga
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+)
+
+const (
+	baseURL = "https://localhost:5665/"
+)
 
 func TestNewClient(t *testing.T) {
-	baseURL := "https://localhost:5665/"
 	c := NewClient(baseURL, nil)
 
 	if got, want := c.BaseURL.String(), baseURL+apiVersion+"/"; got != want {
@@ -11,5 +17,29 @@ func TestNewClient(t *testing.T) {
 	}
 	if got, want := c.UserAgent, userAgent; got != want {
 		t.Errorf("NewClient UserAgent is %v, want %v", got, want)
+	}
+}
+
+func TestNewRequest(t *testing.T) {
+	c := NewClient(baseURL, nil)
+
+	inURL, outURL := "/foo", baseURL+"foo"
+	inBody, outBody := &Host{Name: "u"}, `{"__name":"u"}`+"\n"
+	req, _ := c.NewRequest("GET", inURL, inBody)
+
+	// test that relative URL was expanded
+	if got, want := req.URL.String(), outURL; got != want {
+		t.Errorf("NewRequest(%q) URL is %v, want %v", inURL, got, want)
+	}
+
+	// test that body was JSON encoded
+	body, _ := ioutil.ReadAll(req.Body)
+	if got, want := string(body), outBody; got != want {
+		t.Errorf("NewRequest(q) Body is %v, want %v", got, want)
+	}
+
+	// test that default user-agent is attached to the request
+	if got, want := req.Header.Get("User-Agent"), c.UserAgent; got != want {
+		t.Errorf("NewRequest() User-Agent is %v, want %v", got, want)
 	}
 }
